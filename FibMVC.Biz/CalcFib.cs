fa538254e60f89e99c75b090035ec6a73aa720fa
Stephen Kirkland <sk01417@uga.edu>
2017-02-20 10:47:16 -0500

    progress

---
+++ FibMVC.Biz/CalcFib.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibMVC.Biz
+{
+    public class CalcFib
+    {
+        public List<int> d;
+        public int a = 0;
+        public int b = 0;
+        public List<int> Fib()
+        {
+            d.Add(0);
+            d.Add(1);
+            for (int i = 0; i < 15; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return d;
+        }
+
+        public void Fib(int s, int t)
+        {
+
+        }
+    }
+}
