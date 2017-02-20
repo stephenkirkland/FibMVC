4dae7be9a3f70b94f30668149243669f06f5fb6b
Stephen Kirkland <sk01417@uga.edu>
2017-02-20 10:59:45 -0500

    made the controller and "test" view

---
+++ FibMVC/Controllers/FibController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FibMVC.Controllers
+{
+    public class FibController : Controller
+    {
+        // GET: Fib
+        public ViewResult test()
+        {
+            return View();
+        }
+    }
+}
